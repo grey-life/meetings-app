a96a8356eb12d143791ed2acd82f86c9bc34f0c2
Varier <Aravind.Varier@team.telstra.com>
2020-09-16 18:03:30 +0530

    Change styling for Container component

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Container = ({ children }) => (
-    <div className="container">
+    <div className="container-fluid h-100">
         { children }
     </div>
 );
