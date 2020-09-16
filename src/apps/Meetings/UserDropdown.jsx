606281917e12f565d3fda14c1c8c40099b3ffac2
Varier <Aravind.Varier@team.telstra.com>
2020-09-16 12:47:07 +0530

    Create UserDropdown component

---
+++ src/apps/Meetings/UserDropdown.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+
+const UserDropdown = ({ addAttendee, userEmails }) => {
+    const [selected, setSelected] = useState('');
+    const handleChange = (event) => {
+        setSelected(event.target.value);
+    };
+
+    return (
+        <div className="form-group d-flex justify-content-between">
+            <select
+                id="date"
+                name="date"
+                value={selected}
+                onChange={handleChange}
+            >
+                {
+                    userEmails.map((user) => (
+                        <option
+                            key={user.username}
+                            value={user.username}
+                        >
+                            {user.username}
+                        </option>
+                    ))
+                }
+            </select>
+            <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => { addAttendee(selected); }}
+            >
+                Add participant
+            </button>
+        </div>
+    );
+};
+
+export default UserDropdown;
